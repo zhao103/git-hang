e05e7b2831ec739489a3be365b57ffd89e8dc667
zhao <332177838@qq.com>
2023-09-22 17:15:47 +0800

    idea-git代码测试2.0

--- git-text/src/main/java/Text.java
+++ git-text/src/main/java/Text.java
@@ -1,5 +1,7 @@
 public class Text {
     public static void main(String[] args) {
         System.out.println("这是一git文件");
+        System.out.println("这是一git文件");
+        System.out.println("这是一git文件");
     }
 }
